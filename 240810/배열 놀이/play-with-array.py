d9b857ae7ed4b6fe3a8cf3fb778493cce7c78fe0
매빈 <77741296+aoqlsdl@users.noreply.github.com>
2024-08-10 03:17:58 +0900

    Title: 배열 놀이, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240810/배열 놀이/play-with-array.py
+++ 240810/배열 놀이/play-with-array.py
@@ -7,10 +7,11 @@ for i in range(q):
     if ask[0] == 1:
         print(elem[ask[1] - 1])
     elif ask[0] == 2:
-        if ask[1] in elem:
-            print(elem.index(ask[1]) + 1)
-        else:
+        if ask[1] not in elem:
             print(0)
+        else:
+            print(elem.index(ask[1]) + 1)
     elif ask[0] == 3:
         for i in range(ask[1], ask[2] + 1):
             print(elem[i - 1], end = " ")
+        print()
