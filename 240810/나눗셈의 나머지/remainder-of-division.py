6e0d12c89ee36fbf41932719c2456a532828f66f
매빈 <77741296+aoqlsdl@users.noreply.github.com>
2024-08-10 02:30:26 +0900

    Title: 나눗셈의 나머지, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

--- 240810/나눗셈의 나머지/remainder-of-division.py
+++ 240810/나눗셈의 나머지/remainder-of-division.py
@@ -1,5 +1,5 @@
 a, b = map(int, input().split())
-arr = [0] * (b - 1)
+arr = [0] * b
 hap = 0
 exchange = 0
 
@@ -7,9 +7,9 @@ while True:
     if a <= 1:
         break
     
-    a = a // b
     exchange = a % b
-    arr[exchange - 1] += 1
+    a = a // b
+    arr[exchange] += 1
 
 for a in arr:
     hap += a ** 2
