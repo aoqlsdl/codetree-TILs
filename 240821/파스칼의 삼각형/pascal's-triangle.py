46cad615b9fb646efe4fb0fdfa62a65f94029877
매빈 <77741296+aoqlsdl@users.noreply.github.com>
2024-08-21 01:05:41 +0900

    Title: 파스칼의 삼각형, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

--- 240821/파스칼의 삼각형/pascal's-triangle.py
+++ 240821/파스칼의 삼각형/pascal's-triangle.py
@@ -4,18 +4,21 @@ arr = [
     for i in range(n)
 ]
 
-for i in range(2):
-    for j in range(len(arr[i])):
-        arr[i][j] = 1
 
-for i in range(2, n):
-    for j in range(len(arr[i])):
-        if j == 0 or j == len(arr[i]) - 1:
+if n > 1:
+    for i in range(2):
+        for j in range(len(arr[i])):
             arr[i][j] = 1
-        else:
-            arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j]
-
-for i in range(n):
-    for a in arr[i]:
-        print(a, end = " ")
-    print()
+            
+    for i in range(2, n):
+        for j in range(len(arr[i])):
+            if j == 0 or j == len(arr[i]) - 1:
+                arr[i][j] = 1
+            else:
+                arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j]
+    for i in range(n):
+        for j in range(len(arr[i])):
+            print(arr[i][j], end = " ")
+        print()
+else:
+    print(1)
