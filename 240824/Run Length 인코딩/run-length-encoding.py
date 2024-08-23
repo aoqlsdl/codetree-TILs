77066fee3f09742296a67de6a80a9d8fdb45fdd1
매빈 <77741296+aoqlsdl@users.noreply.github.com>
2024-08-24 04:19:27 +0900

    Title: Run Length 인코딩, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240824/Run Length 인코딩/run-length-encoding.py
+++ 240824/Run Length 인코딩/run-length-encoding.py
@@ -2,17 +2,21 @@ A = input()
 rle = ''
 cnt = 1
 
-for i in range(len(A) - 1):
-    if A[i] == A[i + 1]:
-        cnt += 1
-    else:
-        rle += A[i]
-        rle += str(cnt)
-        cnt = 1
-    
-    if i == len(A) - 2:
-        rle += A[i + 1]
-        rle += str(cnt)
+if len(A) == 1:
+    rle += A
+    rle += str(cnt)
+else:
+    for i in range(1, len(A)):
+        if A[i - 1] == A[i]:
+            cnt += 1
+        else:
+            rle += A[i - 1]
+            rle += str(cnt)
+            cnt = 1
+        
+        if i == len(A) - 1:
+            rle += A[i - 1]
+            rle += str(cnt)
 
 print(len(rle))
 print(rle)
